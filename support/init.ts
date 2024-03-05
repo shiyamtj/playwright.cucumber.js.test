8905dcee93188c0e15dc2a9acf1ec777983606cd
PC-SHIYAMJ\shiya <shiyamtj@gmail.com>
2024-03-06 01:28:54 +1300

    Test

--- support/init.ts
+++ support/init.ts
@@ -2,8 +2,9 @@ const fs = require('fs-extra')
 
 function createFolder(folderName: string) {
   try {
+    const directory: string = __dirname
     fs.emptyDir(folderName)
-    console.log(`Created! Folder ${folderName}`)
+    console.log(`Created! Folder ${folderName} - ${directory}`)
   } catch (err) {
     console.error(`Failed! Folder ${folderName}`)
     console.error(`Error! ${err}`)
